1ad508c8bd98c4dd6213a9eeec4d3a48dfd278c3
Tory Moscaritolo <torymoscaritolo@gmail.com>
2021-04-12 11:43:15 +1000

    serve HTML file

--- myApp.js
+++ myApp.js
@@ -6,7 +6,10 @@ console.log('Hello World')
 
 //Get request to post hello express when you visit the root domain
 app.get('/', function(req, res) {
-    res.send('Hello Express');
+    //res.send('Hello Express');
+
+    //Serve a HTML File
+    res.sendFile(__dirname + '/views/index.html');
   });
 
 
