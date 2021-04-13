b66d80444a59c0cb339fe0a6028cff514af608fa
Tory Moscaritolo <torymoscaritolo@gmail.com>
2021-04-13 16:04:16 +1000

    chain a middleware for a time server

--- myApp.js
+++ myApp.js
@@ -26,6 +26,13 @@ app.get('/json', (req, res) => res.json(
         {"message": process.env.MESSAGE_STYLE === "uppercase" ? response.toUpperCase() : response})
 );
 
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.send({'time':req.time});
+});
+
 app.use('/public', express.static(__dirname + '/public'));
 
 module.exports = app;
