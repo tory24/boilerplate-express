42883ace55f1e31575e0c7bb3351a2fee6f645bc
Tory Moscaritolo <torymoscaritolo@gmail.com>
2021-04-13 16:28:30 +1000

    provide a route parameter input

--- myApp.js
+++ myApp.js
@@ -35,4 +35,8 @@ app.get('/now', function(req, res, next) {
 
 app.use('/public', express.static(__dirname + '/public'));
 
+app.get('/:word/echo', function(req,res) {
+  res.send({'echo':req.params.word});
+});
+
 module.exports = app;
