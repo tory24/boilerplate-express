903c265d8d11fbba38fd6075924009a96eb0bc4c
Tory Moscaritolo <torymoscaritolo@gmail.com>
2021-04-14 13:05:06 +1000

    add post request handler

--- myApp.js
+++ myApp.js
@@ -49,5 +49,9 @@ app.get('/name', function(req,res) {
   res.json({'name': `${req.query.first} ${req.query.last}`})
 });
 
+app.post('/name', function(req,res) {
+  res.json({'name': `${req.body.first} ${req.body.last}`})
+});
+
 
 module.exports = app;
